787a4e8c5630c1b4f00a02b34a05b86d0d19246e
kurogamidesuu <hempushpchauhan@gmail.com>
2025-05-14 17:56:17 +0530

    completed initial project

--- index.js
+++ index.js
@@ -24,7 +24,29 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date?", function (req, res) {
+  const dateInput = req.params.date;
+  let date;
 
+  if(!dateInput) {
+    date = new Date();
+  }else if(!isNaN(dateInput)) {
+    date = new Date(parseInt(dateInput));
+  } else {
+    date = new Date(dateInput);
+  }
+
+  if(date.toString() === "Invalid Date") {
+    res.json({ error: "Invalid Date"});
+  } else {
+    res.json(
+      {
+        unix: date.getTime(),
+        utc: date.toUTCString()
+      }
+    );
+  }
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
